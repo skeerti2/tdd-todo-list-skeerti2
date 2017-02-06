bd65b3c15d795a195a71017f7d08affd0228905b
Sruti Keerti <skeerti2@gmail.com>
2017-02-06 23:21:23 +0800

    assignment

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -2,10 +2,11 @@ const uuidGenerator = require('uuid/v4')
 const fs = require('fs')
 
 const todos = []
-//// the following line will instead load the todos from a json file when the app starts
+// Write functions here
+// // the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
 
-//// The following function can be used to save the todos array to the json data file
+// // The following function can be used to save the todos array to the json data file
 // function save () {
 //   const json = JSON.stringify(todos)
 //   fs.writeFileSync('data.json', json, 'utf8')
@@ -13,13 +14,36 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  if (params.name.length < 5 || params.name.length === 0) {
+    return false
+  } else {
+    params['_id'] = uuidGenerator()
+    // console.log(params['_id'])
+    todos.push(params)
+    return true
+  }
 }
 
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos
 }
+
 function show (id) {
+  var object = null
+  todos.forEach(function(element){
+    // console.log('enetred show function')
+    //  console.log(object['_id'])
+      if(element['_id'] === id){
+      // console.log(id)
+      // console.log(object['_id'])
+      // console.log('entered if loop')
+      object = element
+    }
+  })
+  return object;
+  // console.log(id)
   // find the TODO with this id
 }
 
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -1,5 +1,27 @@
 const assert = require('assert')
 const todos = require('../controllers/todos_controller.js')
 
+// Write tests here
+// need to look at todos
 // // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+ var list2 = {name: "hw", description: "todays assignement", completed: true }
+var list1 = {name: "hisomething", description: "something", completed: true }
+assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+// assert.ok(true)
+// todos.create({name: "hi"})
+assert.deepStrictEqual(todos.create(list1), true, 'obj not created' )
+assert.deepStrictEqual(todos.create(list2), false, 'name is greater than 5') //checks if name.length < 2
+// console.log(todos.list())
+assert.deepStrictEqual(todos.list()[0].hasOwnProperty('_id'), true, 'id doesnt exist')
+// assert.ok(todos.show(todos.list()[0]['_id']))
+var myId = todos.list()[0]['_id']
+console.log(myId)
+console.log(todos.show(myId))
+assert.strictEqual(todos.show(myId),todos.list()[0], 'doesnt return an object' )
+// console.log(todos.list()[0]['_id'])
+// var showOp = todos.show(myId)
+// console.log(showOp)
+// assert.ok(todos.create.params['_id'])
+// assert.deepStrictEqual(todos.show('_id'), '_id', 'it doesnt have an id')
+
+// assert.deepStrictEqual(typeof todos[0]['name'], 'string', 'name not a string')
